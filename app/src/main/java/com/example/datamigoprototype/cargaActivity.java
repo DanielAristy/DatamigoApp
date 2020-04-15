c84c12df960148583b37a271c063b3a453bca7da
DanielAristy <danielariaty@gmail.com>
2020-04-15 18:17:48 -0500

    Implementando Date picker para el activity donde creamos la factura

--- app/src/main/java/com/example/datamigoprototype/cargaActivity.java
+++ app/src/main/java/com/example/datamigoprototype/cargaActivity.java
@@ -3,27 +3,40 @@ package com.example.datamigoprototype;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
-public class cargaActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class cargaActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageView;
+    Button btnFecha;
+    EditText txtPickerFecha;
+    int month, day, year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carga);
         imageView = findViewById(R.id.imgFactura);
+        inicializacion();
+        btnFecha.setOnClickListener(this);
+    }
+
+    private void inicializacion() {
+        btnFecha = findViewById(R.id.btnFecha);
+        txtPickerFecha = findViewById(R.id.txtPickerFecha);
     }
 
     public void loadImage(View view) {
-
         tomarFoto();
     }
     public void selectImage(View view) {
@@ -50,9 +63,27 @@ public class cargaActivity extends AppCompatActivity {
         }
     }
 
-
     public void registrarFactura(View view) {
         Intent registroFactura = new Intent(this,ThreeActivity.class);
         startActivity(registroFactura);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnFecha) {
+            Calendar calendar = Calendar.getInstance();
+            day = calendar.get(Calendar.DAY_OF_MONTH);
+            month = calendar.get(Calendar.MONTH);
+            year = calendar.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(cargaActivity.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    txtPickerFecha.setText(dayOfMonth+"/"+(month + 1)+"/"+year);
+                }
+            }
+            ,day,month,year);
+            datePickerDialog.show();
+        }
     }
 }
