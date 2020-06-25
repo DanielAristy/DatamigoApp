1535230ff8d1dd67e4ebee47f1b6d3c72719dab7
DanielAristy <danielariaty@gmail.com>
2020-06-25 17:43:03 -0500

    Agregando estilos y agreagando un checkBox al segundo activity_second

--- app/src/main/java/com/example/datamigoprototype/SecondActivity.java
+++ app/src/main/java/com/example/datamigoprototype/SecondActivity.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private CheckBox checkBox;
+    String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initComponents();
+    }
+
+    private void initComponents() {
+        checkBox = findViewById(R.id.checkBox);
     }
 
     public void goToActivityLogin(View view) {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+
+    }
+
+    public void loguearChecbBox(View view) {
+        s = "Estado: " + (checkBox.isChecked() ? "Marcado" : "No marcado");
+        Toast.makeText(this, s , Toast.LENGTH_SHORT).show();
     }
 }
